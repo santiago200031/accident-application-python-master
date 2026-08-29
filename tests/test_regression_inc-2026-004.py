3abf0d344a323c4fb699e53640f9a0a3769357c8
Santiago Villavicencio <66896776+santiago200031@users.noreply.github.com>
2026-08-29 16:34:01 +0200

    fix: add file for patch-attempt-1

---
+++ tests/test_regression_inc-2026-004.py
@@ -0,0 +1,17 @@
+from incident_package.controllers.indexing_controller import IndexErrorIncident
+
+def test_fetch_record_at_index_within_range():
+    controller = IndexErrorIncident()
+    records_list = ["record1", "record2", "record3"]
+    target_index = 1
+    assert controller.fetch_record_at_index(records_list, target_index) == "record2"
+
+def test_fetch_record_at_index_out_of_range():
+    controller = IndexErrorIncident()
+    records_list = ["record1", "record2", "record3"]
+    target_index = 5
+    assert controller.fetch_record_at_index(records_list, target_index) == 'N/A'
+
+def test_run_method_with_single_record():
+    controller = IndexErrorIncident()
+    assert controller.run() == 'N/A'
